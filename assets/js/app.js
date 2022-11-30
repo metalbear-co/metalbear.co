6dd0f5424b3123fb9a834d42a5ffcb676a19f7e5
Aviram Hassan <aviramyhassan@gmail.com>
2022-11-30 17:57:18 +0200

    anchor mobile fix

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// This code iterates all links from the header
+// and adds a click event listener that when clicked on the same page as the anchor
+// it will add listen to the off canvas being closed and then scroll to the anchor
+var links = document.querySelectorAll(".offcanvas-body .nav-item .nav-link");
+links.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        if ((this.pathname === window.location.pathname) && (this.host === window.location.host)) {
+            var target = document.getElementById(this.getAttribute("href").split("#")[1]);
+            e.preventDefault();
+            let offCanvas = document.getElementById("offcanvasDoks");
+            offCanvas.addEventListener('hidden.bs.offcanvas', function () {
+                target.scrollIntoView();
+            })
+            document.getElementById("menuDismissButton").click();
+
+        }
+    });
+});
