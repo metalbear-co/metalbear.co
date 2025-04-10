608dba5d5e572bb68bfb2caa29d5bdf5144088e5
Razz4780 <msmolarekg@gmail.com>
2025-04-10 15:04:54 +0200

    Fixed build?

--- mirrord.dev/build.sh
+++ mirrord.dev/build.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$HUGO_ENVIRONMENT" == "production" ]; then
     npm run build;
 else
-    npm run build -- -b ${CF_PAGES_URL}mirrord;
+    npm run build -- -b $CF_PAGES_URL/mirrord;
 fi;
 
 npx pagefind --site public/docs --output-path public/pagefind
