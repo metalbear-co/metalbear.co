0ec11771b616db21b3c57b82d1b45f3aa553998c
Razz4780 <msmolarekg@gmail.com>
2025-04-10 15:38:28 +0200

    ah

--- mirrord.dev/build.sh
+++ mirrord.dev/build.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ "$HUGO_ENVIRONMENT" == "production" ]; then
-    npm run build
+    npm run build -- -e production 
 else
-    npm run build -- -b $CF_PAGES_URL/mirrord -e $HUGO_ENVIRONMENT
+    npm run build -- -b $CF_PAGES_URL/mirrord
 fi;
 
 npx pagefind --site public/docs --output-path public/pagefind
