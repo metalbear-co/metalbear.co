a333d162717fd8c7fe4c83a936f5078ca192f1d3
Aviram Hassan <aviramyhassan@gmail.com>
2025-04-21 20:55:19 +0300

    ..

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -e
 if [ "$HUGO_ENVIRONMENT" == "production" ]; then
     npm run build:with-subsite;
 else
-    npm run build:with-subsite -- -b $CF_PAGES_URL;
+    HUGO_BASE_URL=$CF_PAGES_URL npm run build:with-subsite;
 fi;
