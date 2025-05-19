b6f6014c7b8958c1677bd9d1108286168e704ac7
meowjesty <meowjesty_nyan@outlook.com>
2025-05-19 14:33:21 -0300

    npx pagefind

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ if [ "$HUGO_ENVIRONMENT" == "production" ]; then
 else
     HUGO_BASEURL=$CF_PAGES_URL npm run build:with-subsite;
 fi;
+
+npx pagefind --site public/docs --output-path public/pagefind
