e74bd31cb6fd0223596f6186fcfe91d8eda51c98
meowjesty <meowjesty_nyan@outlook.com>
2025-05-23 16:53:38 -0300

    reintroduce pagefind?

---
+++ assets/js/pagefind.js
@@ -0,0 +1,7 @@
+// Initialize pagefind UI
+window.addEventListener('DOMContentLoaded', (event) => {
+    const searchElement = document.getElementById('search');
+    if (searchElement) {
+        new PagefindUI({ element: "#search", showSubResults: true, pageSize: 5, baseUrl: "/mirrord/", showImages: false });
+    }
+});
