bc54a5e060a161857eb12aa2c56c68e28752eae3
meowjesty <meowjesty_nyan@outlook.com>
2025-06-02 11:52:27 -0300

    remove #

--- assets/js/pagefind.js
+++ assets/js/pagefind.js
@@ -2,6 +2,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const searchElement = document.getElementById('search');
     if (searchElement) {
-        new PagefindUI({ element: "#search", showSubResults: true, pageSize: 5, baseUrl: "/mirrord/", showImages: false });
+        new PagefindUI({
+            element: '#search',
+            showSubResults: true,
+            pageSize: 5,
+            baseUrl: '/mirrord/',
+            showImages: false,
+            processTerm: function(term) {
+                return term.replace(/#.*$/, '');
+            }
+        });
     }
 });
--- mirrord.dev/assets/js/search.js
+++ mirrord.dev/assets/js/search.js
@@ -1,3 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  new PagefindUI({ element: "#search", showSubResults: true, pageSize: 5, baseUrl: "/mirrord/", showImages: false });
+    const searchElement = document.getElementById('search');
+    if (searchElement) {
+        new PagefindUI({
+            element: '#search',
+            showSubResults: true,
+            pageSize: 5,
+            baseUrl: '/mirrord/',
+            showImages: false,
+            processTerm: function(term) {
+                return term.replace(/#.*$/, '');
+            }
+        });
+    }
 });
